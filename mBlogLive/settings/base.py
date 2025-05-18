0241d28b775a308522a71ef8a9b691b5366ff3ed
ManiGit123 <manikandanp045@gmail.com>
2025-05-18 18:25:09 +0530

    postgresql setup

--- mBlogLive/settings/base.py
+++ mBlogLive/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,13 +89,20 @@ WSGI_APPLICATION = "mBlogLive.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    "default": dj_database_url.config(
+        default="postgresql://mblog_user:hKyPIVZcURCP54fsIy2XvRkbomaLuRNl@dpg-d0kq4k56ubrc73bgirhg-a/mblog",
+        conn_max_age=600,
+        ssl_require=True,  # important for Render
+    )
 }
-
+# postgresql://mblog_user:hKyPIVZcURCP54fsIy2XvRkbomaLuRNl@dpg-d0kq4k56ubrc73bgirhg-a.oregon-postgres.render.com/mblog
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
